466d21d8f51acb86a555307894c24e07a32584fb
siner308 <siner308@gmail.com>
2019-03-18 07:56:26 +0900

    fix chromedriver path

--- youtube.py
+++ youtube.py
@@ -21,7 +21,7 @@ def setup_chrome():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("disable-gpu")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Chrome("./chromedriver", chrome_options=chrome_options)
+    driver = webdriver.Chrome("/app/chromedriver", chrome_options=chrome_options)
     time.sleep(5)
     return driver
 
